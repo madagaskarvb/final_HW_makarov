a5a69e3bd5503a6258d43399a8592f33fcc536e1
madagaskarvb <ivanseychelles@gmail.com>
2026-04-16 20:55:27 +0300

    added router for user endpoints

---
+++ internal/routers/user_router.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter
+from internal.models.user_model import User, AddUser
+from internal.handlers import crud_handler
+
+router = APIRouter()
+
+
+@router.get("/", response_model=list[User])
+def get_users():
+    return crud_handler.read_users()
+
+
+@router.get("/{user_id}", response_model=User)
+def get_user(user_id: int):
+    return crud_handler.read_user(user_id)
+
+
+@router.post("/create", response_model=User, status_code=201)
+def post_user(user: AddUser):
+    return crud_handler.create_user(user)
+
+
+@router.put("/{user_id}", response_model=User)
+def put_user(user_id: int, name: str):
+    return crud_handler.update_user(user_id, name)
+
+
+@router.delete("/{user_id}")
+def remove_user(user_id: int):
+    return crud_handler.delete_user(user_id)
